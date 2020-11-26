873a212ffea0072c7d79ef75a413933635346a4b
BuildTools <leokeviny@gmail.com>
2020-11-26 16:26:14 +0700

    [Kevin] Fix error mode

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,5 +1,12 @@
-@extends('errors::minimal')
+@extends('errors::illustrated-layout')
 
-@section('title', __('Service Unavailable'))
+@section('title', __('Under Maintenance'))
 @section('code', '503')
+
+@section('image')
+<div style="background-image: url('../img/error-bg.jpg');"
+    class="absolute pin bg-no-repeat sm:bg-center md:bg-center lg:bg-center">
+</div>
+@endsection
+
 @section('message', __($exception->getMessage() ?: 'Service Unavailable'))
